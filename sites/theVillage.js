1ff8ed3b4dc5b4da31ceee8192a46dc6fa9e3c78
alexaaaant <alexaaaant@gmail.com>
2019-12-01 17:51:34 +0300

    Реализовано получение конкретной статьи

--- sites/theVillage.js
+++ sites/theVillage.js
@@ -13,19 +13,39 @@ function getArticles(theme, count) {
         .then((res) => res.text())
         .then((html) => {
             const doc = new DOMParser().parseFromString(html, 'text/html');
-            let articles = doc.getElementsByClassName('post-link');
-            articles = [...articles,].slice(0, count + 4);
-            articles = articles.map((article) => (article.pathname));
-            articles = [...new Set(articles),];
-            const list = document.getElementsByClassName('articles')[0];
+            let articles = doc.getElementsByClassName('post-block-featured');
+            articles = [...articles,].slice(0, count);
+
+            const articlesObj = new Map();
+
             articles.forEach((article) => {
+                const tmpArticle = article.firstChild;
+                const title = tmpArticle.children[1].firstChild.textContent;
+                articlesObj.set(title, {
+                    link: `${url}${tmpArticle.pathname}`,
+                    preamble: tmpArticle.children[1].lastChild.textContent,
+                });
+            });
+
+            const list = document.getElementsByClassName('articles')[0];
+            articlesObj.forEach((article, title) => {
                 const newEl = document.createElement('button');
-                newEl.innerText = article;
-                newEl.className='articles__item';
+                newEl.innerText = title;
+                newEl.className = 'articles__item';
+                newEl.onclick = () => getArticle(article.link);
                 list.appendChild(newEl);
             });
         })
         .catch((err) => console.log(err));
 }
 
-getArticles(themes[1], 10);
+function getArticle(link) {
+    fetch(link)
+        .then((res) => res.text())
+        .then((html) => {
+            const doc = new DOMParser().parseFromString(html, 'text/html');
+        })
+        .catch((err) => console.log(err));
+}
+
+getArticles(themes[0], 10);
