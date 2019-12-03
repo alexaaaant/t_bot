8e52289d5ba759ecaf997afafcd64c511d7c98e7
alexaaaant <alexaaaant@gmail.com>
2019-12-03 08:41:35 +0300

    Реализован рендер статей

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ function getUpdate() {
     fetch(`${url}/getUpdates`)
         .then((res) => res.json())
         .then((updates) => {
-            console.log(updates);
             updates.result.forEach((update) => {
                 if (update.message) {
                     const { id, first_name, } = update.message.chat;
                     chats.set(id, first_name);
                 }
+                // sendMessageEveryone();
             });
         })
         .catch((err) => console.log(err));
@@ -21,13 +21,13 @@ function getUpdate() {
 
 function sendMessageEveryone() {
     chats.forEach((personName, chat_id) => {
-        const text = `${personName}, интересная статистика.`;
+        const text = `${personName}, *ночь*`;
         sendMessage(chat_id, text);
     });
 }
 
 function sendMessage(chat_id, text) {
-    fetch(encodeURI(`${url}/sendMessage?chat_id=${chat_id}&text=${text}`))
+    fetch(encodeURI(`${url}/sendMessage?chat_id=${chat_id}&text=${text}&parse_mode=markdown`))
         .then((res) => res.json())
         .then((data) => console.log(data))
         .catch((err) => console.log(err));
@@ -40,5 +40,17 @@ function getMe() {
         .catch((err) => console.log(err));
 }
 
+function renderArticles(articles) {
+    const list = document.getElementsByClassName('articles')[0];
+    articles.forEach((article, title) => {
+        const newEl = document.createElement('button');
+        newEl.innerText = title;
+        newEl.className = 'articles__item';
+        // newEl.onclick = () => getArticle(article.link);
+        list.appendChild(newEl);
+    });
+}
+
 // getUpdate();
-TheVillage.getArticles(TheVillage.themes[2],15);
+TheVillage.getArticles(TheVillage.themes[2], 15)
+    .then(articles => renderArticles(articles));
