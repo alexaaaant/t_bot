7e4b318ba955859b5a0d087617c6453c175d170f
alexaaaant <alexaaaant@gmail.com>
2019-12-07 14:40:54 +0300

    Реализован компонент формы

--- frontend/src/components/date.js
+++ frontend/src/components/date.js
@@ -1,53 +1,68 @@
 class DateComponent {
     constructor() {
-        this.popup = null;
+        this.form = null;
         this.r = new RegExp(/(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]) (([01][0-9])|(2[0-3])):[0-5][0-9]$/, 'i',);
     }
     render(x, y, ) {
-        this.popup = this.createPopup();
-        this.popup.style.left = `${x}px`;
-        this.popup.style.top = `${y}px`;
-        document.body.appendChild(this.popup,);
-        this.popup.focus();
-        this.popup.addEventListener('input', (e, ) => this.changeInput(e,),);
+        this.form = this.createForm();
+        this.form.style.left = `${x}px`;
+        this.form.style.top = `${y}px`;
+        document.body.appendChild(this.form,);
+        // this.form.focus();
+        // this.form.addEventListener('input', (e, ) => this.changeInput(e,),);
+    }
+
+    createForm() {
+        const form = document.createElement('form',);
+        const inputDate = document.createElement('input',);
+        const inputTime = document.createElement('input',);
+        const submitButton = document.createElement('button',);
+
+        form.className = 'form-date d-flex flex-column';
+        inputDate.className = 'form-date__input';
+        inputDate.placeholder = 'Дата';
+        inputTime.className = 'form-date__input';
+        inputTime.placeholder = 'Время';
+        submitButton.type = 'submit';
+        submitButton.className = 'form-date__button';
+        submitButton.textContent = 'Принять';
+
+        form.appendChild(inputDate,);
+        form.appendChild(inputTime,);
+        form.appendChild(submitButton,);
+        return form;
     }
 
     changeInput(e, ) {
-        if (this.r.test(this.popup.value,) && !this.popup.classList.contains('valid',)) {
-            this.popup.classList.add('valid',);
-            this.popup.classList.remove('invalid',);
+        if (this.r.test(this.form.value,) && !this.form.classList.contains('valid',)) {
+            this.form.classList.add('valid',);
+            this.form.classList.remove('invalid',);
         }
-        if (!this.r.test(this.popup.value,) && !this.popup.classList.contains('invalid',)) {
-            this.popup.classList.add('invalid',);
-            this.popup.classList.remove('valid',);
+        if (!this.r.test(this.form.value,) && !this.form.classList.contains('invalid',)) {
+            this.form.classList.add('invalid',);
+            this.form.classList.remove('valid',);
         }
-    }
-
-    createPopup() {
-        const popup = document.createElement('input',);
-        popup.className = 'popup';
-        return popup;
     }
 
     checkValid() {
-        if (this.popup !== null) {
-            return this.popup.classList.contains('valid',);
+        if (this.form !== null) {
+            return this.form.classList.contains('valid',);
         }
     }
 
     getDate() {
-        return new Date(this.popup.value,);
+        return new Date(this.form.value,);
     }
 
     setUnRenderHandler(handler, ) {
-        this.popup.addEventListener('blur', () => {
+        this.form.addEventListener('blur', () => {
             handler();
             this.unRender();
         },);
     }
 
     unRender() {
-        document.body.removeChild(this.popup,);
+        document.body.removeChild(this.form,);
     }
 
 }
