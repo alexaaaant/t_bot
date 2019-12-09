d39fef92323d53b11d26f815b4df8835a1341aca
alexaaaant <alexaaaant@gmail.com>
2019-12-09 21:55:50 +0300

    Перенесен таск для удаления

--- backend/routers/tBot.js
+++ backend/routers/tBot.js
@@ -1,48 +1,46 @@
 import Router from 'koa-router';
 import request from 'request-promise';
 import dotenv from 'dotenv';
-import { exec } from 'child_process';
+import { exec, } from 'child_process';
 
 dotenv.config();
 
 const url = `${process.env.T_BOT_ADRESS}/bot${process.env.T_BOT_TOKEN}`;
 
-const router = new Router({ prefix: '/bot', });
+const router = new Router({ prefix: '/bot', },);
 
 router
     .get('/getMe', async (ctx, ) => {
-        const res = await request(`${url}/getMe`);
+        const res = await request(`${url}/getMe`,);
         ctx.body = res;
-    });
+    },);
 
 router
     .get('/getUpdates', async (ctx, ) => {
-        const res = await request(`${url}/getUpdates?offset=${ctx.request.query.offset}&timeout=100`);
-        Object.assign(ctx, { body: res, });
-        const updates = JSON.parse(res);
+        const res = await request(`${url}/getUpdates?offset=${ctx.request.query.offset}&timeout=100`,);
+        Object.assign(ctx, { body: res, },);
+        const updates = JSON.parse(res,);
         let lastUpdateID = 0;
         if (updates.ok === true && updates.result.length) {
             updates.result.forEach((update, ) => {
                 const text = `${update.message.from.first_name}, пока что единственное, что я могу сказать абсолютно точно, что вареники с вишней лучше чем с картошкой`;
-                request(encodeURI(`http://localhost:3000/api/bot/sendMessage?chat_id=${update.message.chat.id}&text=${text}`));
-            });
+                request(encodeURI(`http://localhost:3000/api/bot/sendMessage?chat_id=${update.message.chat.id}&text=${text}`,),);
+            },);
             lastUpdateID = updates.result[updates.result.length - 1].update_id + 1;
         }
-        request(`http://localhost:3000/api/bot/getUpdates?offset=${lastUpdateID}`);
-    });
+        request(`http://localhost:3000/api/bot/getUpdates?offset=${lastUpdateID}`,);
+    },);
 
 router
     .get('/sendMessage', async (ctx, ) => {
-        const { chat_id, text, task } = ctx.request.query;
+        const { chat_id, text, task, } = ctx.request.query;
 
-        await request(encodeURI(`${url}/sendMessage?chat_id=${chat_id}&text=${text}&parse_mode=markdown`));
+        await request(encodeURI(`${url}/sendMessage?chat_id=${chat_id}&text=${text}&parse_mode=markdown`,),);
 
-        exec(`schtasks /delete /tn "${task}" /f`,
-        (e, stdout, stderr) => console.log('errors', e, stdout, stderr));
+        request(`http://localhost:${process.env.PORT}/api/task/delete?${task}`,);
+        Object.assign(ctx, { body: 'Send success!', },);
+    },);
 
-        Object.assign(ctx, { body: 'Send success!', });
-    });
-
-request(`http://localhost:${process.env.PORT}/api/bot/getUpdates`);
+request(`http://localhost:${process.env.PORT}/api/bot/getUpdates`,);
 
 export default router;
--- backend/routers/tasks.js
+++ backend/routers/tasks.js
@@ -1,11 +1,16 @@
 import Router from 'koa-router';
-import { exec } from 'child_process';
+import { exec, } from 'child_process';
 
-const router = new Router({ prefix: '/task' });
+const router = new Router({ prefix: '/task', },);
 
-router.get('/plan', async (ctx) => {
-exec('SCHTASKS /CREATE /SC ONCE /TN MY_TASK_888 /TR "powershell Invoke-WebRequest http://localhost:3000/api/bot/sendMessage?chat_id=9408538&text=hui&task=MY_TASK_888 -UseBasicParsing" /SD 07.12.2019 /ST 17:53',
-    (e, stdout, stderr) => console.log('errors', e, stdout, stderr));
-})
+router.get('/plan', async () => {
+    exec('SCHTASKS /CREATE /SC ONCE /TN MY_TASK_888 /TR "powershell Invoke-WebRequest http://localhost:3000/api/bot/sendMessage?chat_id=9408538&text=hui&task=MY_TASK_888 -UseBasicParsing" /SD 07.12.2019 /ST 17:53',
+        (e, stdout, stderr, ) => console.log('errors', e, stdout, stderr,),);
+},);
+
+router.get('/delete', async (ctx, ) => {
+    exec(`schtasks /delete /tn "${ctx.request.query.task}" /f`,
+        (e, stdout, stderr, ) => console.log('errors', e, stdout, stderr,),);
+},);
 
 export default router;
