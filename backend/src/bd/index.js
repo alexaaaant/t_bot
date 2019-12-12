8fa21844e5e9bff11390ac045479f9cdf49aa23d
alexaaaant <alexaaaant@gmail.com>
2019-12-12 21:58:33 +0300

    Реализован запрос на добавление строк в базу

--- backend/src/bd/index.js
+++ backend/src/bd/index.js
@@ -4,16 +4,27 @@ const pool = new Pool;
 
 const getAllMessages = () => {
     return pool.query('select * from messages',)
-    .then((res,) => {
-        return res.rows;
+        .then((res, ) => {
+            return res.rows;
 
-    },)
-    .catch((err,) => {
-        console.log(err,);
-    },);
+        },)
+        .catch((err, ) => {
+            return err;
+        },);
 };
 
+const insertMessage = (taskId, chatId, text, date, ) => {
+    return pool.query(`INSERT INTO messages VALUES
+    (${taskId},${chatId}, '${text}', '${date}')`,)
+        .then((res, ) => {
+            return res;
+        },)
+        .catch((err, ) => {
+            return err;
+        },);
+};
 
 export {
     getAllMessages,
+    insertMessage,
 };
