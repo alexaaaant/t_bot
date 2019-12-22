b18c36ad7e720aa473add6feb1de301e62f23606
alexaaaant <alexaaaant@gmail.com>
2019-12-22 12:24:26 +0300

    Вынесена функция создания колонки

--- frontend/src/containers/messages.js
+++ frontend/src/containers/messages.js
@@ -5,37 +5,28 @@ class Messages {
         const container = document.createElement('div',);
         container.className = 'messages d-flex';
 
-        const { messagesList: messagesListTodo, messages: todoMessages, } = this.createColumn('To do',);
-        const { messagesList: messagesListDone, messages: doneMessages, } = this.createColumn('Done',);
-
-        messages.forEach((message, ) => {
-            const messageComponent = new Message(message.text, message.date, message.status,);
-            const messageElement = messageComponent.render();
-            const status = messageComponent.getData().status;
-
-            if (status === '0') {
-                messagesListTodo.appendChild(messageElement,);
-            }
-            if (status === '1') {
-                messagesListDone.appendChild(messageElement,);
-            }
-        },);
-        todoMessages.appendChild(messagesListTodo,);
-        doneMessages.appendChild(messagesListDone,);
+        const { messagesElement: todoMessages, } = this.createColumn('To do', messages.filter((message, ) => message.status === '0',),);
+        const { messagesElement: doneMessages, } = this.createColumn('Done', messages.filter((message, ) => message.status === '1',),);
 
         container.appendChild(todoMessages,);
         container.appendChild(doneMessages,);
         document.body.appendChild(container,);
     }
-    createColumn(name, ) {
-        const messages = document.createElement('div',);
+    createColumn(name, messages, ) {
+        const messagesElement = document.createElement('div',);
         const header = document.createElement('h4',);
-        messages.className = 'messages__container';
+        messagesElement.className = 'messages__container';
         header.innerText = name;
-        messages.appendChild(header,);
+        messagesElement.appendChild(header,);
         const messagesList = document.createElement('div',);
         messagesList.className = 'messages__list';
-        return { messagesList, messages, };
+        messages.forEach((message, ) => {
+            const messageComponent = new Message(message.text, message.date, message.status,);
+            messagesList.appendChild(messageComponent.render(),);
+        },);
+        messagesElement.appendChild(messagesList,);
+
+        return { messagesElement, };
     }
 }
 
