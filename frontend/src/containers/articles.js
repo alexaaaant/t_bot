9365e60734794d2119defd860217dc734f0b4cd8
alexaaaant <alexaaaant@gmail.com>
2019-12-29 12:20:33 +0300

    Удалены лишние функции

--- frontend/src/containers/articles.js
+++ frontend/src/containers/articles.js
@@ -1,4 +1,3 @@
-import FormComponent from '../components/form/form';
 import Message from '../components/message';
 import Column from '../components/column';
 
@@ -12,8 +11,6 @@ class Articles {
         articles.forEach(async (article, title, ) => {
             this.allArticles.set(title, article,);
             const newEl = await this.createArticleElement(title,);
-            // newEl.onclick = (e, ) => this.selectArticle(e, title,);
-            // list.appendChild(newEl,);
             this.column.addMessage(newEl,);
         },);
     }
@@ -25,21 +22,6 @@ class Articles {
     async createArticleElement(title, ) {
         const text = await this.createText(title,);
         return new Message(text,);
-    }
-
-    selectArticle(e, title, ) {
-        const coords = e.currentTarget.getBoundingClientRect();
-        const y = (window.scrollY + coords.top + coords.height) - 5;
-        const x = (window.scrollX + coords.left + coords.width) - 5;
-        this.form = new FormComponent();
-        this.form.render(x, y,);
-        // this.date.setSubmitHandler(() => this.planTask(e.target, title,),);
-    }
-
-    static doneTask(id, ) {
-        const article = document.getElementById(id,);
-        article.classList.remove('planned',);
-        article.classList.add('done',);
     }
 
     async createText(title, ) {
